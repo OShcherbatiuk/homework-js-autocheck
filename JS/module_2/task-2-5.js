1d846fd899f6b4d5e91a922f383b6f560b45b23d
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2020-12-16 23:16:49 +0200

    upd

--- JS/module_2/task-2-5.js
+++ JS/module_2/task-2-5.js
@@ -3,7 +3,7 @@ const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
 // Пиши код ниже этой строки
 const firstElement = fruits[0];
 const secondElement = fruits[1];
-const lastElement = fruits[3];
+const lastElement = fruits[fruits.length -1];
 
 
 // Доступ к элементам по индексу
