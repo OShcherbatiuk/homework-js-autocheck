e65ff028e2ee476cd4de2f356cffb288f5787195
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-01-13 00:37:28 +0200

    upd

--- JS/module_3/task-3-26.js
+++ JS/module_3/task-3-26.js
@@ -1,3 +1,19 @@
+// Пиши код ниже этой строки
+function calculateMeanTemperature(forecast) {
+
+  const { 
+  today: {
+    low: todayLow, 
+    high: todayHigh, 
+  }, 
+  tomorrow: {
+    low: tomorrowLow, 
+    high: tomorrowHigh,  
+  } 
+} = forecast;
+  // Пиши код выше этой строки
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
 
 
 
